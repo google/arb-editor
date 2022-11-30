28d05fdb630c13a25e7723f60eda43c69192d33b
Moritz <mosum@google.com>
2022-11-30 16:23:56 +0100

    add license headers

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// Copyright 2022 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 import * as vscode from 'vscode';
 import { ConfigurationTarget, workspace } from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 	const configuration = workspace.getConfiguration('files');
 	// eslint-disable-next-line @typescript-eslint/naming-convention
-	configuration.update('associations', { "*.arb": "json" }, ConfigurationTarget.Global).then(() => {
-		// take action here
-	});
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "arbeditor" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('arbeditor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage(configuration.get('associations')!);
-	});
-
-	context.subscriptions.push(disposable);
+	await configuration.update('associations', { "*.arb": "json" }, ConfigurationTarget.Global);
 }
 
 // This method is called when your extension is deactivated
