a3f19acddcbde6809b9eacf61c323805a39baed0
Moritz <mosum@google.com>
2022-12-14 13:00:40 +0100

    add ICU syntax support for highlighting

--- src/extension.ts
+++ src/extension.ts
@@ -11,6 +11,18 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+'use strict';
+
+import path = require('path');
+import { visit } from 'jsonc-parser';
+
+module.exports = {
+	activate
+};
+
+let pendingFooJsonDecoration: string | number | NodeJS.Timeout | undefined;
+
+import { debug } from 'console';
 import * as vscode from 'vscode';
 import { ConfigurationTarget, workspace } from 'vscode';
 
@@ -20,7 +32,124 @@ export async function activate(context: vscode.ExtensionContext) {
 	const configuration = workspace.getConfiguration('files');
 	// eslint-disable-next-line @typescript-eslint/naming-convention
 	await configuration.update('associations', { "*.arb": "json" }, ConfigurationTarget.Global);
+
+	// decorate when changing the active editor editor
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => {
+		if (editor !== undefined) {
+			return updateFooJsonDecorations(editor);
+		}
+	}, null, context.subscriptions));
+
+	// decorate when the document changes
+	context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(event => {
+		if (vscode.window.activeTextEditor && event.document === vscode.window.activeTextEditor.document) {
+			if (pendingFooJsonDecoration) {
+				clearTimeout(pendingFooJsonDecoration);
+			}
+			const activeTextEditor = vscode.window.activeTextEditor;
+			if (activeTextEditor !== undefined) {
+				pendingFooJsonDecoration = setTimeout(() => updateFooJsonDecorations(activeTextEditor), 1000);
+			}
+		}
+	}, null, context.subscriptions));
+
+	// decorate the active editor now
+	const activeTextEditor = vscode.window.activeTextEditor;
+	if (activeTextEditor !== undefined) {
+		updateFooJsonDecorations(activeTextEditor);
+	}
+
+	// decorate when then cursor moves
+	context.subscriptions.push(new EditorEventHandler());
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
+
+const argDecoration = vscode.window.createTextEditorDecorationType({
+	light: {
+		color: '#ff6f00'
+	},
+	dark: {
+		color: '#fff9c4'
+	}
+});
+const selectDecoration = vscode.window.createTextEditorDecorationType({
+	light: {
+		color: '#6a1b9a'
+	},
+	dark: {
+		color: '#ce93d8'
+	}
+});
+const pluralDecoration = vscode.window.createTextEditorDecorationType({
+	light: {
+		color: '#0277bd'
+	},
+	dark: {
+		color: '#b3e5fc'
+	}
+});
+
+const selectRegex = /^(\w+\s*,\s*select\s*,(?:\s*\w+\{.*\})*)$/g;
+const pluralRegex = /^(\w+\s*,\s*plural\s*,(?:\s*\w+\{.*\})*)$/g;
+function updateFooJsonDecorations(editor: vscode.TextEditor) {
+	if (!editor || !path.basename(editor.document.fileName).endsWith('.arb')) {
+		return;
+	}
+	let colorMap = new Map<string, vscode.Range[]>();
+	visit(editor.document.getText(), {
+		onLiteralValue: (value: string, offset: number) => {
+			let starts: number[] = [];
+			for (let index = 0; index < value.length; index++) {
+				const element = value.charAt(index);
+				if (element === '{') {
+					starts.push(index);
+				} else if (element === '}') {
+					let start = starts.pop()!;
+					let end = index;
+					const part = value.substring(start + 1, end);
+					debug(part);
+					const level = starts.length > 1 ? 1 : 0;
+					const decoration = parse(part, level);
+					if (decoration !== undefined) {
+						const rangeStart = offset + start + 1;
+						const rangeEnd = offset + end + 2;
+						colorMap.set(decoration, [...(colorMap.get(decoration) ?? []), new vscode.Range(editor.document.positionAt(rangeStart), editor.document.positionAt(rangeEnd))]);
+					}
+				}
+			}
+		}
+	});
+	editor.setDecorations(pluralDecoration, colorMap.get('plural') ?? []);
+	editor.setDecorations(selectDecoration, colorMap.get('select') ?? []);
+	editor.setDecorations(argDecoration, colorMap.get('arg') ?? []);
+}
+
+function parse(value: string, level: number): string | undefined {
+	if (selectRegex.exec(value) !== null) {
+		return 'select';
+	} else if (pluralRegex.exec(value) !== null) {
+		return 'plural';
+	} else {
+		return 'arg';
+	}
+}
+
+class EditorEventHandler {
+	private _disposable: vscode.Disposable;
+
+	constructor() {
+		let subscriptions: vscode.Disposable[] = [];
+		vscode.window.onDidChangeTextEditorSelection((e: vscode.TextEditorSelectionChangeEvent) => {
+			if (e.textEditor === vscode.window.activeTextEditor) {
+				updateFooJsonDecorations(e.textEditor);
+			}
+		}, this, subscriptions);
+		this._disposable = vscode.Disposable.from(...subscriptions);
+	}
+
+	dispose() {
+		this._disposable.dispose();
+	}
+}
