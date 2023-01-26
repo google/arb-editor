c8e409e3fc0a93e0721e54e35bbaadb712facb4b
Moritz <mosum@google.com>
2023-01-26 16:07:03 +0100

    Add snippets only for `arb` files

--- src/extension.ts
+++ src/extension.ts
@@ -20,9 +20,10 @@ module.exports = {
 
 let pendingDecorations: NodeJS.Timeout | undefined;
 
+import path = require('path');
 import * as vscode from 'vscode';
 import { DecoratorAndParser } from './parseAndDecorate';
-
+const snippetsJson = require("../snippets/snippets.json");
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
@@ -48,6 +49,17 @@ export async function activate(context: vscode.ExtensionContext) {
 		}
 	}, null, context.subscriptions));
 
+	// Make the snippets available in arb files
+	const completions = getSnippets();
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
+		{ language: 'json', pattern: `**/*.arb` },
+		{
+			provideCompletionItems(document, position, token, context) {
+				return completions;
+			}
+		},
+	));
+
 	// decorate the active editor now
 	const activeTextEditor = vscode.window.activeTextEditor;
 	if (activeTextEditor !== undefined) {
@@ -56,6 +68,25 @@ export async function activate(context: vscode.ExtensionContext) {
 
 
 	// At extension startup
+}
+
+function getSnippets() {
+	const completions = new vscode.CompletionList();
+	const snippets = snippetsJson as { [key: string]: { prefix: string; description: string; body: string[]; }; };
+	for (const snippetType of Object.keys(snippets)) {
+		const snippet = snippets[snippetType];
+
+		const completionItem = new vscode.CompletionItem(snippet.prefix, vscode.CompletionItemKind.Snippet);
+		completionItem.filterText = snippet.prefix;
+		completionItem.insertText = new vscode.SnippetString(
+			Array.isArray(snippet.body)
+				? snippet.body.join("\n")
+				: snippet.body
+		);
+		completionItem.detail = snippet.description;
+		completions.items.push(completionItem);
+	}
+	return completions;
 }
 
 // This method is called when your extension is deactivated
