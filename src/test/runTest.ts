ed0bb10b7fb1be1f9e772a6880aff02a86955f40
Moritz <mosum@google.com>
2023-01-27 11:48:27 +0100

    Add ability to regenerate goldens

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -24,7 +24,6 @@ async function main() {
 		// The path to test runner
 		// Passed to --extensionTestsPath
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
-
 		// Download VS Code, unzip it and run the integration test
 		await runTests({ extensionDevelopmentPath, extensionTestsPath });
 	} catch (err) {
