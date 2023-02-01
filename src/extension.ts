a268b0278c82ca4dffd49b4a99bc04819259300e
Moritz <mosum@google.com>
2023-02-01 14:28:14 +0100

    Add diagnostic codes

--- src/extension.ts
+++ src/extension.ts
@@ -27,6 +27,7 @@ import { Diagnostics } from './diagnose';
 import { Parser, StringMessage } from './messageParser';
 const snippetsJson = require("../snippets/snippets.json");
 const snippetsInlineJson = require("../snippets/snippets_inline.json");
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
@@ -51,7 +52,7 @@ export async function activate(context: vscode.ExtensionContext) {
 			diagnostics.diagnose(editor, messageList, errors);
 		}
 	}, null, context.subscriptions));
-
+	
 	// decorate when the document changes
 	context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(event => {
 		if (vscode.window.activeTextEditor && event.document === vscode.window.activeTextEditor.document) {
@@ -117,3 +118,24 @@ function getSnippets(snippetsJson: any): vscode.CompletionList {
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
+export class QuickFixes implements vscode.CodeActionProvider {
+
+	public static readonly providedCodeActionKinds = [
+		vscode.CodeActionKind.QuickFix
+	];
+
+	provideCodeActions(document: vscode.TextDocument, range: vscode.Range | vscode.Selection, context: vscode.CodeActionContext, token: vscode.CancellationToken): vscode.CodeAction[] {
+		// for each diagnostic entry that has the matching `code`, create a code action command
+		return context.diagnostics
+			.filter(diagnostic => diagnostic.code === EMOJI_MENTION)
+			.map(diagnostic => this.createCommandCodeAction(diagnostic));
+	}
+
+	private createCommandCodeAction(diagnostic: vscode.Diagnostic): vscode.CodeAction {
+		const action = new vscode.CodeAction('Learn more...', vscode.CodeActionKind.QuickFix);
+		action.command = { command: COMMAND, title: 'Learn more about emojis', tooltip: 'This will open the unicode emoji page.' };
+		action.diagnostics = [diagnostic];
+		action.isPreferred = true;
+		return action;
+	}
+}
