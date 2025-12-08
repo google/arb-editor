cf0cea6fcc6c00a7870524157a70679dff699436
Moritz <mosum@google.com>
2025-12-08 10:47:55 +0100

    More upgrades

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -13,9 +13,9 @@
 // limitations under the License.
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import { glob } from 'glob';
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
 	// Create the mocha test
 	const mocha = new Mocha({
 		ui: 'tdd',
@@ -24,28 +24,28 @@ export function run(): Promise<void> {
 
 	const testsRoot = path.resolve(__dirname, '..');
 
-	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
+	return new Promise<void>(async (c, e) => {
+		try {
+			const files = await glob('**/**.test.js', { cwd: testsRoot });
 
 			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+			files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
 
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				console.error(err);
+			// Run the mocha test
+			mocha.run(failures => {
+				if (failures > 0) {
+					e(new Error(`${failures} tests failed.`));
+				} else {
+					c();
+				}
+			});
+		} catch (err: unknown) {
+			console.error(err);
+			if (err instanceof Error) {
 				e(err);
+			} else {
+				e(new Error(`An unknown error occurred during test setup: ${String(err)}`));
 			}
-		});
+		}
 	});
 }
