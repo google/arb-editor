e33255fdbf2a53fab7ea339966cdc9d48ca3c43e
Moritz <mosum@google.com>
2025-12-08 11:09:43 +0100

    Update deps (#85)
    * Update
    
    * More upgrades
    
    * update node version in CI
    
    * add changelog
    
    * upgrade

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import * as tsEslint from "@typescript-eslint/eslint-plugin";
+
+export default [
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+            ecmaVersion: 6,
+            sourceType: "module",
+        },
+        ignores: [
+            "out",
+            "dist",
+            "**/*.d.ts"
+        ],
+    },
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: tsParser,
+        },
+        plugins: {
+            "@typescript-eslint": tsEslint.default || tsEslint.plugin,
+        },
+        rules: {
+            "curly": "warn",
+            "eqeqeq": "warn",
+            "no-throw-literal": "warn",
+            "@typescript-eslint/naming-convention": "warn",
+            "semi": "off",
+        }
+    }
+];
